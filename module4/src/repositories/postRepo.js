92d944381e04b57f44975a934bda7fde13901114
Justin-Madren <justinmadren765@gmail.com>
2026-02-05 11:52:21 -0500

    Module 4 creation

---
+++ module4/src/repositories/postRepo.js
@@ -0,0 +1,45 @@
+import { posts, getNextId } from '../db/posts.js';
+
+
+export function getAll(){
+    return posts;
+}
+
+export function getById(id){
+    let post = posts.find(p => p.id === id);
+    return post;
+}
+
+export function create(postData){
+    const newPost = {
+        id: getNextId(),
+        title: postData.title,
+        content: postData.content,
+        createAt: new Date().toISOString(),
+    };
+    posts.push(newPost);
+    return newPost;
+}
+
+export function update(id, postData){
+    const post = posts.find(p => p.id === id);
+    if(!post){
+        return undefined;
+    }
+    if(updatedData.title){
+        post.title = postData.title;
+    }
+    if(postData.content){
+        post.content = postData.content;
+    }
+    return post;
+}
+
+export function remove(id){
+    const index = posts.findIndex(p => p.id === id);
+    if(index === -1){
+        return false;
+    }
+    posts.splice(index, 1);
+    return true;
+}
---
+++ module4/src/services/postService.js
@@ -0,0 +1,43 @@
+import { getAll,getById,create,update,remove } from "../repositories/postRepo.js";
+
+
+export function getAllPosts(){
+    return getAll();
+}
+
+export function getPostById(id){
+    const post =  getById(id);
+    if(post){
+        return post;
+    }else{
+        const error = new Error(`Post ${id} not found`);
+        error.status = 404;
+        throw error;
+    }
+}
+
+export function createPost(postData){
+    return create(postData);
+}
+
+export function updatePost(id, postData){
+    const updatedPost = update(id, postData);
+    if(updatedPost){
+        return updatedPost;
+    }else{
+        const error = new Error(`Post ${id} not found`);
+        error.status = 404;
+        throw error;
+    }
+}
+
+export function deletePost(id){
+    const result = remove(id);
+    if(result){
+        return;
+    }else{
+        const error = new Error(`Post ${id} not found`);
+        error.status = 404;
+        throw error;
+    }
+}
